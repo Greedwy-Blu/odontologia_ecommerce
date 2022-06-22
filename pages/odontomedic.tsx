173ccc42abb85de5a42b714e7d1cd8746e5ee196
Gabriel Oliveira <greedwyblu@gmail.com>
2022-06-22 01:29:06 -0300

    mobile

--- pages/odontomedic.tsx
+++ pages/odontomedic.tsx
@@ -23,13 +23,13 @@ export default  class odontomedic extends React.Component{
         <main className={styles.containerAnimacao}>
         <Nav/>
      
-     <div className="flex justify-center text-center lg:ml-56 lg:pl-60 mt-24 sm:m-8 fixed">
+     <div className="flex justify-center text-center items-center lg:ml-56 lg:pl-60 mt-24 sm:m-8 fixed">
 
-<h1 className="text-4xl  py-4 lg:pl-4 lg:mt-8 sm:pl-2">Sobre a odontemedic</h1>
+<h1 className="text-4xl  py-4 lg:pl-4 lg:mt-8 sm:ml-4 ">Sobre a odontemedic</h1>
 
             </div> 
 
-            <div className="pl-12 lg:translate-y-12">
+            <div className="pl-12 mt-20 fixed">
 <button className="flex justify-end lg:mt-24   sm:12  bg-green-500 h-10 w-10 rounded-full">
   <IconContext.Provider value={{ size: "2em", className: "flex items-center" }}>
     <span className="mt-0.5 p-0.5"><BiLeftArrowAlt/></span>
@@ -38,16 +38,16 @@ export default  class odontomedic extends React.Component{
      </div>
      
 
-      <div className="flex lg:justify-start sm:justify-center mt-36 pt-24 lg:ml-24 sm:-translate-y-8">
+      <div className="flex lg:justify-start sm:justify-center mt-36 pt-24 lg:ml-24 ">
 
-      <span className="animate-waving-hand animate-bounce ">  <Image src={imCardOdont} className="" height={560} width={520} /></span>
+      <span className="animate-waving-hand animate-bounce  ">  <Image src={imCardOdont} className="" height={560} width={520} /></span>
 
       </div>
       
           
-  <div className="flex  lg:justify-end sm:justify-center m-4  lg:pl-56 lg:ml-40 mt-36 pt-36 sm:-translate-x-24  ">
+  <div className="flex  lg:justify-end sm:justify-center m-4   lg:pl-56 lg:ml-40 mt-36 pt-36   ">
  
-      <p className="lg:text-2xl sm:text-xl antialiased hover:subpixel-antialiased    ">A odontomedic foi criado para fornecer ajuda  aos profissionais e as pessoas  que nescessita equipamentos medicos á produtos medicos </p>
+      <p className="lg:text-2xl sm:text-2xl antialiased hover:subpixel-antialiased    ">A odontomedic foi criado para fornecer ajuda  aos profissionais e as pessoas  que nescessita equipamentos medicos á produtos medicos </p>
     
 
       </div>
